8511c09b8136e98f95f41e2f4aad5ffd246c9835
lanshi <yzs20030317@gmail.com>
2025-08-26 02:39:52 +0800

    feat: 实现一个简单的文件搜索程序
    - 添加解析命令行参数的功能
    - 实现读取文件内容并打印
    - 新增 Config 结构体和 Parse trait
    - 添加 poem.txt 文件作为测试

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+//main.rs
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config = Config::parse_config(&args);
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    let contents = fs::read_to_string(&config.file_path).expect("Should have been able to read the file");
+    println!("with text:\n{contents}");
+}
+
+
+trait Parse {
+    fn parse_config(args: &[String]) -> Self;
+}
+struct Config {
+    query: String,
+    file_path: String,
+}
+impl Parse for Config {
+    fn parse_config(args: &[String]) -> Self {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Self { query, file_path }
+    }
 }
